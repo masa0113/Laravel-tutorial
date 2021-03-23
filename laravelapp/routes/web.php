c768368f300f29633154492df855bf852abb07bc
masahiro.fukuyama <be.fukuyama.m@gmail.com>
2021-03-23 17:34:16 +0900

    setting vue-Laravel tutorial

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -14,11 +14,16 @@ use App\Http\Controllers\HelloController;
 |
 */
 
-Route::get('/',  fn() => view('welcome'));
+//Route::get('/',  fn() => view('welcome'));
+
+// 下記はvue-Laravel用
+Route::get('/{any}', fn() => view('app'))->where('any', '.*');
+
+// 下記はLaravel入門用
 
 // Laravel8から以下の書き方に変更となった
 // Route::get('hello', 'HelloController@index');
 
 // Route::get('hello', [HelloController::class, 'index']);
 // Route::get('hello/other', [HelloController::class, 'other']);
-Route::get('hello', [HelloController::class, '__invoke']);
+// Route::get('hello', [HelloController::class, '__invoke']);
