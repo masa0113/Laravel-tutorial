8d0d309a4d91b960ba96b57e954d28de51b69ff4
masahiro.fukuyama <be.fukuyama.m@gmail.com>
2021-03-23 18:39:59 +0900

    commit refactor

--- laravelapp/resources/js/app.js
+++ laravelapp/resources/js/app.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import HeaderComponent from "./components/HeaderComponent";
 import TaskListComponent from "./components/TaskListComponent";
+import TaskShowComponent from "./components/TaskShowComponent";
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -22,6 +23,13 @@ const router = new VueRouter({
             path: '/tasks',
             name: 'task.list',
             component: TaskListComponent
+        },
+
+        {
+            path: '/tasks/:taskId',
+            name: 'task.show',
+            component: TaskShowComponent,
+            props: true
         }
     ]
 });
