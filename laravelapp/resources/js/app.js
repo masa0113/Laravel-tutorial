4b8e1f10cc76e772b998925e49d612743aeed2db
masahiro.fukuyama <be.fukuyama.m@gmail.com>
2021-07-11 13:47:10 +0900

    Vue側作成

--- laravelapp/resources/js/app.js
+++ laravelapp/resources/js/app.js
@@ -3,6 +3,8 @@ import VueRouter from "vue-router";
 import HeaderComponent from "./components/HeaderComponent";
 import TaskListComponent from "./components/TaskListComponent";
 import TaskShowComponent from "./components/TaskShowComponent";
+import TaskCreateComponent from "./components/TaskCreateComponent.vue";
+import TaskEditComponent from "./components/TaskEditComponent.vue"
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -26,9 +28,22 @@ const router = new VueRouter({
         },
 
         {
+            path: '/tasks/create',
+            name: 'task.create',
+            component: TaskCreateComponent,
+        },
+
+        {
             path: '/tasks/:taskId',
             name: 'task.show',
             component: TaskShowComponent,
+            props: true
+        },
+
+        {
+            path: '/tasks/:taskId/edit',
+            name: 'task.edit',
+            component: TaskEditComponent,
             props: true
         }
     ]
--- laravelapp/resources/js/components/HeaderComponent.vue
+++ laravelapp/resources/js/components/HeaderComponent.vue
@@ -4,10 +4,12 @@
             <nav class="navbar navbar-dark">
                 <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span>
                 <div>
-                    <router-link v-bind:to="{name: 'task.list'}">
+                    <router-link :to="{ name: 'task.list' }">
                         <button class="btn btn-success">List</button>
                     </router-link>
-                    <button class="btn btn-success">ADD</button>
+                    <router-link :to="{ name: 'task.create' }">
+                        <button class="btn btn-success">ADD</button>
+                    </router-link>
                 </div>
             </nav>
         </div>
@@ -15,5 +17,5 @@
 </template>
 
 <script>
-    export default {}
+export default {};
 </script>
